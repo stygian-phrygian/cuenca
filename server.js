8f233d33b2033acad78106dd5d0d8906c13c2709
Josh W <this is my email ******>
2016-10-23 21:53:00 -0400

    added function key support to server.js

--- server.js
+++ server.js
@@ -105,7 +105,18 @@ var KEY_TO_XKEY = {
     'x': 'x',
     'y': 'y',
     'z': 'z',
-
+    'F1': 'F1',
+    'F2': 'F2',
+    'F3': 'F3',
+    'F4': 'F4',
+    'F5': 'F5',
+    'F6': 'F6',
+    'F7': 'F7',
+    'F8': 'F8',
+    'F9': 'F9',
+    'F10': 'F10',
+    'F11': 'F11',
+    'F12': 'F12'
 }
 
 function hasModifierKeys(keyStroke) {
